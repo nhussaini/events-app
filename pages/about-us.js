b93c9a410c32090771a57e976bd8d5917ed09dfc
nhussaini <nasrullah_hussaini@yahoo.com>
2023-03-08 12:28:06 -0500

    create dynamic routes

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,5 +1,18 @@
 const Page = () => {
-  return <h1>About us page</h1>;
+  return (
+    <div>
+      <h1>About us Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sed viverra tellus
+        in hac habitasse platea. Sit amet purus gravida quis blandit turpis
+        cursus in. Duis at tellus at urna condimentum mattis pellentesque id. Ut
+        sem nulla pharetra diam sit. In vitae turpis massa sed elementum tempus
+        egestas sed sed. Ac feugiat sed lectus vestibulum mattis ullamcorper
+        velit sed. Luctus venenatis lectus magna fringilla urna. Aliquet eget
+      </p>
+    </div>
+  );
 };
 
 export default Page;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,23 @@
 const Page = () => {
-  return <h1>Events page</h1>;
+  return (
+    <div>
+      <h1>Events Page</h1>
+      <div>
+        <a href="">
+          <img />
+          Events in London
+        </a>
+        <a href="">
+          <img />
+          Events in San Fransisco
+        </a>
+        <a href="">
+          <img />
+          Events in Paris
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
