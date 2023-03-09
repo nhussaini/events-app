59548d442ae0fc0d80191484023653a5872b45e4
nhussaini <nasrullah_hussaini@yahoo.com>
2023-03-08 22:15:55 -0500

    use getStaticPaths with getStaticProps

--- pages/events/[cat]/index.js
+++ pages/events/[cat]/index.js
@@ -1,15 +1,54 @@
-const EventsCatPage = () => {
+import Image from 'next/image';
+
+const EventsCatPage = ({ data }) => {
   return (
     <div>
       <h1>Events in London</h1>
 
-      <a href="/events/event1">Event 1</a>
+      {data.map((ev) => (
+        <a key={ev.id} href={`/events/${ev.city}/${ev.id}`}>
+          <Image width={300} height={300} alt={ev.title} src={ev.image} />
+          <h2>{ev.title}</h2>
+          <p>{ev.description}</p>
+        </a>
+      ))}
+
+      {/* <a href="/events/event1">Event 1</a>
       <a href="/events/event2">Event 2</a>
       <a href="/events/event3">Event 3</a>
       <a href="/events/event4">Event 4</a>
-      <a href="/events/event5">Event 5</a>
+      <a href="/events/event5">Event 5</a> */}
     </div>
   );
 };
 
 export default EventsCatPage;
+
+export async function getStaticPaths() {
+  const { events_categories } = await import('/data/data.json');
+
+  const allPaths = events_categories.map((ev) => {
+    return {
+      params: {
+        cat: ev.id.toString(),
+      },
+    };
+  });
+  // console.log(allPaths);
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context?.params.cat;
+  const { allEvents } = await import('/data/data.json');
+  const data = allEvents.filter((ev) => ev.city === id);
+  // console.log(data);
+  return {
+    props: {
+      data: data,
+    },
+  };
+}
