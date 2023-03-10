b80b3132418a9b9545514bd5859c5ed8fa49600a
nhussaini <nasrullah_hussaini@yahoo.com>
2023-03-10 15:27:48 -0500

    creaet a simple form for registration

--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -1,10 +1,16 @@
 import Image from 'next/image';
 const SingleEvent = ({ data }) => {
+  const onSubmit = () => {};
   return (
     <div>
       <Image src={data.image} width={500} height={300} alt={data.title}></Image>
       <h1>{data.title}</h1>
       <p>{data.description}</p>
+      <form onSubmit={onSubmit}>
+        <label>Get Registered for this event!</label>
+        <input type="email" id="email" placeholder="Please insert your email" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
